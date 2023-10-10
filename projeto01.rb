e9af36d61f64e89377faf746fb37ad9c7588fb3e
Psandrea <129814302+Psandrea@users.noreply.github.com>
2023-10-10 17:59:03 -0300

    Add files via upload

---
+++ projeto01.rb
@@ -0,0 +1,10 @@
+puts 'Bem vindo(a) à Biblioteca Dev!'
+puts "Digite seu nome: "
+nome = gets.chomp
+puts "Digite seu sobremone: "
+sobrenome = gets.chomp
+puts "informe a sua idade: "
+idade = gets.chomp.to_i
+puts "Olá, seu nome é: #{nome} #{sobrenome} e você tem: #{idade} anos"
+
+
